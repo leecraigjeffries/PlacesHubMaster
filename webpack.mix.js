60c03d37c3890b988ca95efb43eb5fefc943b36a
Lee Jeffries <leecjeffries@gmail.com>
2019-06-07 21:36:55 +0100

    npm install basic JS files webpack.mix edited

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendor/jquery.js')
+    .copy('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'public/js/vendor/bootstrap.js')
+    .copy('node_modules/jsrender/jsrender.min.js', 'public/js/vendor/jsrender.js')
+    .copy('node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js', 'public/js/vendor/data-tables-bs4.js')
+    .copy('node_modules/flatpickr/dist/flatpickr.min.js', 'public/js/vendor/flatpickr.js')
+    .copy('node_modules/datatables.net/js/jquery.dataTables.min.js', 'public/js/vendor/data-tables.js')
+    .sass('resources/sass/app.scss', 'public/css/app')
+    .sass('resources/sass/dev.scss', 'public/css/app')
+    .scripts(['resources/js/throttle-debounce.js', 'resources/js/app.js'], 'public/js/app/app.js');
